8b7554e8bae2e9cb9539dfc7ccc04cfb9063029f
Neharsha <kannegantivishnu0904@gmail.com>
2024-08-01 00:44:38 +0530

    2 pages done

--- store/views.py
+++ store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Product
+def home(request):
+    products=Product.objects.all()
+    return render(request,'store/home.html',{'products':products})
